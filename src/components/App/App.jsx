36b57321d3c0b8c60f3a17df2468c917b6c971b1
NataliiaShah <gavrilovskaboroluk@gmail.com>
2024-12-21 15:36:52 +0200

    SearchBar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,16 +6,30 @@ import Loader from "../Loader/Loader";
 import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 import SearchBar from "../SearchBar/SearchBar";
 
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
+
+    const [searchQuery, setSearchQuery] = useState("");
+    
+    const handleSearchSubmit = (query) => {
+    setSearchQuery(query);
+    };
+    
     return (
         <>
+             
+            <SearchBar onSubmit={handleSearchSubmit} />
+            <Toaster position="top-center" /> 
+            <h2>Search results for: {searchQuery}</h2>
+      
             <ErrorMessage />;
             <ImageCard />;
             <ImageGallery />;
             <ImageModal />;
             <Loader />;
             <LoadMoreBtn />;
-            <SearchBar/>;
         </>
     );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,52 @@
+import { Formik, Form, Field } from 'formik';
+import toast from "react-hot-toast";
 
-const SearchBar = () => {
-    return (
-        <>
-        </>
-    );
+const SearchBar = ({ onSubmit }) => {
+  return (
+    <>
+      <header>
+        <Formik 
+          initialValues={{ query: "" }} 
+          validate={(values) => {
+            const errors = {};
+            if (!values.query) {
+              errors.query = "Please enter a search term to find images."; 
+            }
+            return errors;
+          }}
+          onSubmit={(values, actions) => {
+            if (!values.query.trim()) {
+              toast.error("Please enter a search term to find images.");
+              actions.setSubmitting(false);
+              return;
+            }
+
+            onSubmit(values.query); 
+            actions.resetForm(); 
+          }}
+        >
+          {({ values, handleChange, handleSubmit, errors, touched, isSubmitting }) => (
+            <Form onSubmit={handleSubmit}>
+              <Field
+                type="text"
+                name="query"
+                value={values.query}
+                onChange={handleChange}
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+              />
+              {touched.query && errors.query && (
+                <div style={{ color: "red" }}>{errors.query}</div> 
+              )}
+              <button type="submit" disabled={isSubmitting}>Search</button>
+            </Form>
+          )}
+        </Formik>
+      </header>
+    </>
+  );
 };
 
 export default SearchBar;
+
