19b769ddd995dd6ee11614c6099a7d8a1664a70e
NataliiaShah <gavrilovskaboroluk@gmail.com>
2024-12-22 21:23:38 +0200

    correction

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,26 +1,7 @@
 import Modal from 'react-modal';
 
-// Встановлюємо елемент для доступності
-Modal.setAppElement('#root');
 
-// Стилі для модального вікна
-const modalStyles = {
-  content: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'transparent',
-    border: 'none',
-    padding: 0,
-    maxWidth: '90%',
-    maxHeight: '90%',
-    overflow: 'hidden',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.8)', // темний фон за межами модалки
-  },
-};
+Modal.setAppElement('#root');
 
 const ImageModal = ({ isOpen, closeModal, imageUrl, imageAlt }) => {
   console.log('isOpen:', isOpen, 'imageUrl:', imageUrl, 'imageAlt:', imageAlt);  // Лог для перевірки пропсів
@@ -29,11 +10,10 @@ const ImageModal = ({ isOpen, closeModal, imageUrl, imageAlt }) => {
     <Modal
       isOpen={isOpen}
       onRequestClose={closeModal}
-      style={modalStyles}
-      ariaHideApp={false} // Вимикає warning при використанні React Modal
-      shouldCloseOnOverlayClick={true} // Закриття при кліку за межами
-      onKeyDown={(e) => e.key === 'Escape' && closeModal()} // Закриття при натисканні ESC
-    >
+      
+      ariaHideApp={false} 
+      shouldCloseOnOverlayClick={true} 
+      onKeyDown={(e) => e.key === 'Escape' && closeModal()}>
       <div>
         {imageUrl ? (
           <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
