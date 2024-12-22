6bd6851735d3ddfccf32a75831f2828c9da24a4c
NataliiaShah <gavrilovskaboroluk@gmail.com>
2024-12-22 14:55:19 +0200

    All changes

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-const ErrorMessage = ({ message }) => {
-  return (
-    <div style={{ color: 'red', textAlign: 'center', fontSize: '18px', marginTop: '20px' }}>
-      <strong>Error:</strong> {message}
-    </div>
-  );
-};
-
-export default ErrorMessage;
